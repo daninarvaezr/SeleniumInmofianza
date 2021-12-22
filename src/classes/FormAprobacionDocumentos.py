93c0cba9a122152abaf9aea0a45d9baadde7640f
daninarvaezr <daninarvaezr2835@gmail.com>
2021-12-21 22:30:31 -0500

    Avance rechazo de Formularios

--- src/classes/FormAprobacionDocumentos.py
+++ src/classes/FormAprobacionDocumentos.py
@@ -5,29 +5,18 @@ import time
 from functions.Functions import Functions as Selenium
 from classes.FormLogin import EventLogin
 class EventAprobarDocumentos(unittest.TestCase):
-    def AprobacionDocumento(self):                
+    def AprobacionDocumento(self):
         Selenium.get_json_file(self,"AprobacionDocumentos")
-        Selenium.get_elements(self,"Revisar").click()        
-        #Selenium.close_windows_name(self,'ViewDocPDF')               
+        Selenium.get_elements(self,"Revisar").click()
+        #Selenium.close_windows_name(self,'ViewDocPDF')
         Selenium.switch_to_windows_name(self,'OMNICANALIDAD')
-        Selenium.esperar(self,3)                   
-        Selenium.get_elements(self,"Seleccionar").click()                            
+        Selenium.esperar(self,3)
+        Selenium.get_elements(self,"Seleccionar").click()
         Selenium.capturar_pantalla(self)
         Selenium.get_elements(self,"Siguiente").click()
         Selenium.get_elements(self,"BtnAceptar").click()
         Selenium.esperar(self,3000000)
-    def RechazarDocumento(self):        
-        EventTC.AceptarTratamientoDatos(self)
-        Selenium.get_json_file(self,"AprobacionDocumentos")
-        Selenium.get_elements(self,"Revisar").click()        
-        #Selenium.close_windows_name(self,'ViewDocPDF')               
-        Selenium.switch_to_windows_name(self,'OMNICANALIDAD')
-        Selenium.esperar(self,3)                   
-        Selenium.get_elements(self,"Seleccionar").click()                            
-        Selenium.capturar_pantalla(self)
-        Selenium.get_elements(self,"Siguiente").click()
-        Selenium.get_elements(self,"BtnAceptar").click()
-        Selenium.esperar(self,3000000)        
         
-        #Selenium.esperar(self,1000000)
+    def RechazarDocumento(self):
 
+        #Selenium.esperar(self,1000000)
